2527cd11cade9c3a61b34f30a8b70fafe33a72a3
Suresh-Istina <istinasuresh28@gmail.com>
2024-09-04 11:00:32 +0530

    backend configuration

---
+++ LibraryManagementAPI/Data/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagementAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementAPI.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options)
+            : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+
+   
+}
